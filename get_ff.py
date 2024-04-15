12bfae88e74372c29bcad2be66b4742bf0ae02df
paullabonne <labonnepaul@gmail.com>
2024-04-15 12:35:09 +0200

    formatted code and use a relative path

--- get_ff.py
+++ get_ff.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 import numpy as np
-import pandas as pd
 import pyarrow as pa
 import pyarrow.feather as feather
 import pyarrow.parquet as pq
@@ -12,29 +11,29 @@ import sys
 from datetime import datetime
 import os
 
-os.chdir("Documents/rpyd3f")
+os.chdir(os.path.dirname(__file__))
 
-# date_ref = feather.read_table('data/dates').to_pandas()["date_ref"]
-# date_ref = feather.read_table('data/dates_missing').to_pandas()["missing_days"]
-date_ref = feather.read_table('data/dates_update').to_pandas()["date_ref"]
+# load the dates to scrap
+date_ref = feather.read_table("data/dates").to_pandas()["date_ref"]
 
 try:
-  with open('data/html_source.parquet', 'rb') as f:
-    arrow_source = pq.read_table(f)
-    dates = arrow_source.column(0).to_pylist()
-    html_sources = arrow_source.column(1).to_pylist()
+    with open("data/html_source.parquet", "rb") as f:
+        arrow_source = pq.read_table(f)
+        dates = arrow_source.column(0).to_pylist()
+        html_sources = arrow_source.column(1).to_pylist()
 except:
-  html_sources = []
-  dates = []
+    html_sources = []
+    dates = []
 
 # launching the browser
 from seleniumbase import Driver
-options = webdriver.ChromeOptions() 
+
+options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 driver = Driver(uc=True, incognito=True)
 
 ### these are alternative webdrivers
-#import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
 ## drver = uc.Chrome(driver_executable_path = "chromedriver", options=options)
 # driver.set_page_load_timeout(30000)
 
@@ -44,37 +43,44 @@ driver = Driver(uc=True, incognito=True)
 ###
 
 # forex factor partial url
-ff_url = 'https://www.forexfactory.com/calendar?day='
+ff_url = "https://www.forexfactory.com/calendar?day="
 
-for day in date_ref :
-  # url of the day
-  ff_day_url = ff_url + day
-  
-  # go to the day url
-  driver.get(ff_day_url)
-  time.sleep(1)
-  print(ff_day_url)
-  
-  # find events
-  calandar_elem_closed = driver.find_elements(By.CSS_SELECTOR, ".calendar__cell > [title='Open Detail']")
-    
-  # open details section for each event
-  while len(calandar_elem_closed)>0 :
-    for i in range(0,len(calandar_elem_closed)) :
-      driver.execute_script("arguments[0].click();", calandar_elem_closed[i]) 
-      time.sleep(0.1)
+for day in date_ref:
+    # url of the day
+    ff_day_url = ff_url + day
 
-    # check if some are still closed
-    calandar_elem_closed = driver.find_elements(By.CSS_SELECTOR, ".calendar__cell > [title='Open Detail']")
+    # go to the day url
+    driver.get(ff_day_url)
 
-  time.sleep(1)  
-  # store source html
-  html_sources.append(driver.page_source)
-  dates.append(day)
+    # wait for the page to be fully loaded
+    time.sleep(1)
+    print(ff_day_url)
+
+    # find events
+    calandar_elem_closed = driver.find_elements(
+        By.CSS_SELECTOR, ".calendar__cell > [title='Open Detail']"
+    )
+
+    # open details section for each event
+    while len(calandar_elem_closed) > 0:
+        for i in range(0, len(calandar_elem_closed)):
+            driver.execute_script("arguments[0].click();", calandar_elem_closed[i])
+            time.sleep(0.1)
+
+        # check if some are still closed
+        calandar_elem_closed = driver.find_elements(
+            By.CSS_SELECTOR, ".calendar__cell > [title='Open Detail']"
+        )
+
+    time.sleep(1)
+    # store source html
+    html_sources.append(driver.page_source)
+    dates.append(day)
 
 driver.quit()
 
 arrow_source = pa.Table.from_arrays([dates, html_sources], names=["date", "source"])
 
-with open('data/html_source.parquet', 'wb') as f:
-  pq.write_table(arrow_source, f, compression='brotli')
+# save
+with open("data/html_source.parquet", "wb") as f:
+    pq.write_table(arrow_source, f, compression="brotli")
